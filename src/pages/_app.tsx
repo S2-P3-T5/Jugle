7c2d8503822e93a62b6300343e394dfe6d039b82
dkile <dkile532@naver.com>
2024-01-18 15:28:25 +0900

    Feat: 프로젝트 기본 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,39 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+
+import { fontSpoqaHanSansNeo } from "@/styles/fonts";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <style jsx global>{`
+          html {
+            font-family: ${fontSpoqaHanSansNeo.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </HydrationBoundary>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
---
+++ src/styles/fonts.ts
@@ -0,0 +1,22 @@
+import localFont from "next/font/local";
+
+export const fontSpoqaHanSansNeo = localFont({
+	src: [
+		{
+			path: "../../static/fonts/SpoqaHanSansNeo-Regular.woff2",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../../static/fonts/SpoqaHanSansNeo-Medium.woff2",
+			weight: "500",
+			style: "normal",
+		},
+		{
+			path: "../../static/fonts/SpoqaHanSansNeo-Bold.woff2",
+			weight: "700",
+			style: "normal",
+		},
+	],
+	variable: "--font-spoqa",
+});
