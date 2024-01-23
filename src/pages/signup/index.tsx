b15a6a5f2bf137344cd66e62c0096ef828d58c5f
dkile <dkile532@naver.com>
2024-01-23 12:16:06 +0900

    feat: 회원가입 폼 성공 시 동작 구현

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,107 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useSignup } from "@/queries/user";
+import { SignupForm } from "@/types/user";
+
+export default function Page() {
+  const form = useForm<SignupForm>({
+    values: { email: "", password: "", passwordConfirm: "", type: "employee" },
+  });
+  const { mutate } = useSignup();
+
+  const onSubmit: SubmitHandler<SignupForm> = ({ email, password, type }) => {
+    mutate({ email, password, type });
+  };
+
+  return (
+    <section>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>이메일</FormLabel>
+                <FormControl>
+                  <Input placeholder="이메일을 입력해주세요." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>비밀번호</FormLabel>
+                <FormControl>
+                  <Input placeholder="비밀번호를 입력해주세요." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>비밀번호 확인</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="비밀번호를 다시 입력해주세요."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>회원 유형</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    defaultValue={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <FormItem>
+                      <FormControl>
+                        <RadioGroupItem value="employee" />
+                      </FormControl>
+                      <FormLabel>알바님</FormLabel>
+                    </FormItem>
+                    <FormItem>
+                      <FormControl>
+                        <RadioGroupItem value="employer" />
+                      </FormControl>
+                      <FormLabel>사장님</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">가입하기</Button>
+        </form>
+      </Form>
+    </section>
+  );
+}
