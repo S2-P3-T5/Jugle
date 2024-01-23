a90802ce2d106d754767b374da965589d5076e9f
yspark <ys1008p@gmail.com>
2024-01-23 12:21:17 +0900

    feat: shop/[id].tsx 페이지 ShopRegister 컴포넌트 연결 구현

---
+++ src/pages/shop/[id].tsx
@@ -0,0 +1,7 @@
+import ShopRegister from "@/components/ui/shop/ShopRegister";
+
+export default function Shop() {
+  const hasShopsInfo = false; // 기능 구현 전 임시 설정
+
+  return !hasShopsInfo && <ShopRegister />;
+}
