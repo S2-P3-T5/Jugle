1f6aa7d888228cb683120b5969185a0bc79f0469
eonpain <zmfltmwnsrl@naver.com>
2024-01-24 18:22:17 +0900

    refactor: form 태그 하나로 줄이기

--- src/pages/notice/register/index.tsx
+++ src/pages/notice/register/index.tsx
@@ -18,25 +18,25 @@ function NoticeRegister() {
           />
         </div>
       </Link>
-      <div className="w-[37.5rem] gap-[0.8rem] bg-[#FAFAFA]">
-        <div className="flex flex-col items-center justify-center">
-          <div className="flex w-[35.1rem] flex-col items-start gap-[0.8rem] px-[1.2rem] pb-[8rem] pt-[4rem]">
-            <div className="flex h-[52.3rem] w-full flex-col items-center gap-[2.4rem]">
-              <div className="flex w-full items-center justify-between self-stretch">
-                <h3 className="text-[2rem] font-bold not-italic leading-normal text-black">
-                  공고 등록
-                </h3>
-                <div className="relative h-[2.4rem] w-[2.4rem]">
-                  <Image
-                    src="/icons/close.svg"
-                    layout="fill"
-                    objectFit="contain"
-                    alt="닫기이미지"
-                  />
+      <form>
+        <div className="w-[37.5rem] gap-[0.8rem] bg-[#FAFAFA]">
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex w-[35.1rem] flex-col items-start gap-[0.8rem] px-[1.2rem] pb-[8rem] pt-[4rem]">
+              <div className="flex h-[52.3rem] w-full flex-col items-center gap-[2.4rem]">
+                <div className="flex w-full items-center justify-between self-stretch">
+                  <h3 className="text-[2rem] font-bold not-italic leading-normal text-black">
+                    공고 등록
+                  </h3>
+                  <div className="relative h-[2.4rem] w-[2.4rem]">
+                    <Image
+                      src="/icons/close.svg"
+                      layout="fill"
+                      objectFit="contain"
+                      alt="닫기이미지"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex h-[52.3rem] w-full flex-col justify-end gap-[2rem]">
-                <form>
+                <div className="flex h-[52.3rem] w-full flex-col justify-end gap-[2rem]">
                   <div className="flex h-[9.2rem] w-full flex-col items-start gap-[0.8rem]">
                     <Label htmlFor="시급">시급*</Label>
                     <div className="relative inline-block w-full">
@@ -71,19 +71,19 @@ function NoticeRegister() {
                       className="flex h-[15.3rem] w-full items-center self-stretch overflow-auto rounded-md border border-input px-[2rem] py-[1.6rem] text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                     />
                   </div>
-                </form>
-              </div>
-              <div>
-                <Button className="flex h-[2rem] items-center justify-center gap-[0.8rem] self-stretch rounded-md bg-[#EA3C12] px-[13.6rem] py-[1.4rem]">
-                  <span className="text-center text-lg font-bold leading-5 text-white">
-                    등록하기
-                  </span>
-                </Button>
+                </div>
+                <div>
+                  <Button className="flex h-[2rem] items-center justify-center gap-[0.8rem] self-stretch rounded-md bg-[#EA3C12] px-[13.6rem] py-[1.4rem]">
+                    <span className="text-center text-lg font-bold leading-5 text-white">
+                      등록하기
+                    </span>
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </>
   );
 }
