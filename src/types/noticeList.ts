40371f32ab55cb603be30eeac96bbf72c7349ee2
uzleem <uzleem.github.com>
2024-01-25 06:10:16 +0900

    feat : 공고리스트 타입 추가

---
+++ src/types/noticeList.ts
@@ -0,0 +1,24 @@
+export type Item = {
+  id: string;
+  hourlyPay: number;
+  startsAt: string;
+  workhour: number;
+  description: string;
+  closed: boolean;
+};
+
+export type ShopItem = {
+  id: string;
+  name: string;
+  category: string;
+  address1: string;
+  address2: string;
+  description: string;
+  imageUrl: string;
+  originalHourlyPay: number;
+};
+
+export type Shop = {
+  item: ShopItem;
+  href: string;
+};
