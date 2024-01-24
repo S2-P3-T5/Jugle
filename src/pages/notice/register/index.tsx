b978a0d1e748595cd08890952d0a738687e8530a
eonpain <zmfltmwnsrl@naver.com>
2024-01-25 00:07:27 +0900

    design: shadcn/ui에서 alert-dialog 컴포넌트를 활용해 모달창 UI 다시 구현

--- src/pages/notice/register/index.tsx
+++ src/pages/notice/register/index.tsx
@@ -2,7 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
-import Modal from "@/components/noticeRegister/registerModal";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -120,24 +128,39 @@ function NoticeRegister() {
                       name="explanation"
                     />
                   </div>
-                </div>
-                <div>
-                  <Button
-                    type="button"
-                    className="flex h-[2rem] items-center justify-center gap-[0.8rem] self-stretch rounded-md bg-primary px-[13.6rem] py-[1.4rem]"
-                    onClick={handleRegisterButtonClick}
-                  >
-                    <span className="text-center text-lg font-bold leading-5 text-white">
-                      등록하기
-                    </span>
-                  </Button>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button
+                        type="button"
+                        className="flex h-[2rem] items-center justify-center gap-[0.8rem] self-stretch rounded-md bg-primary px-[13.6rem] py-[1.4rem]"
+                      >
+                        <span className="text-center text-lg font-bold leading-5 text-white">
+                          등록하기
+                        </span>
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent className="flex h-[22rem] w-[32.7rem] flex-col items-center justify-center gap-[5rem] rounded-md border border-none bg-white py-[2.8rem]">
+                      <AlertDialogHeader>
+                        <AlertDialogTitle className="text-[1.6rem] font-medium not-italic leading-normal text-[#333236]">
+                          등록이 완료되었습니다.
+                        </AlertDialogTitle>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogAction className="flex h-[4.2rem] w-[13.8rem] items-center justify-center gap-[1rem] rounded-md bg-primary px-[5.6rem] py-[1.2rem]">
+                          <span className="text-center text-[1.4rem] font-medium not-italic leading-normal text-white">
+                            확인
+                          </span>
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </form>
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
+      {/* <Modal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
     </div>
   );
 }
