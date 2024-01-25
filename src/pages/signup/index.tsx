a616e65bb7e75ad28a889b07659020af8882c8a2
dkile <dkile532@naver.com>
2024-01-25 17:38:28 +0900

    feat: 로고 클릭 시 공고 리스트 화면으로 이동

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import SignupForm from "@/components/signup/SignupForm";
 import DialogProvider from "@/providers/DialogProvider";
+import { PAGE_ROUTES } from "@/routes";
 
 export default function Page() {
   return (
     <DialogProvider>
       <section>
+        <Link href={PAGE_ROUTES.NOTICES}>
+          <Image
+            src="/icons/logo.svg"
+            alt="로고 이미지"
+            width={248}
+            height={48}
+          />
+        </Link>
         <SignupForm />
       </section>
     </DialogProvider>
