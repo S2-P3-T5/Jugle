5cf2b2e5c679667796360464a93d9b35a3946d23
dkile <dkile532@naver.com>
2024-01-30 02:23:46 +0900

    feat: ky adapter 구현

--- src/apis/fetcher.ts
+++ src/apis/fetcher.ts
@@ -1,7 +1,48 @@
-import ky from "ky";
+import ky, { Input, KyInstance, Options } from "ky";
 
 import { API_ROUTE } from "@/routes";
 
-export const fetcher = ky.create({
-  prefixUrl: API_ROUTE,
-});
+interface Fetcher extends Omit<KyInstance, "extend" | "create" | "stop"> {
+  extend: (options: Options) => void;
+}
+
+class KyAdapter implements Fetcher {
+  #fetcher: KyInstance;
+
+  constructor(fetcher: KyInstance) {
+    this.#fetcher = fetcher;
+  }
+
+  get(url: Input, options?: Options) {
+    return this.#fetcher.get(url, options);
+  }
+
+  post(url: Input, options?: Options) {
+    return this.#fetcher.post(url, options);
+  }
+
+  put(url: Input, options?: Options) {
+    return this.#fetcher.put(url, options);
+  }
+
+  delete(url: Input, options?: Options) {
+    return this.#fetcher.delete(url, options);
+  }
+
+  patch(url: Input, options?: Options) {
+    return this.#fetcher.patch(url, options);
+  }
+
+  head(url: Input, options?: Options) {
+    return this.#fetcher.head(url, options);
+  }
+
+  extend(options: Options) {
+    this.#fetcher = this.#fetcher.extend(options);
+    if (process.env.NODE_ENV === "development")
+      // eslint-disable-next-line no-console
+      console.log("ky options extended: ", options);
+  }
+}
+
+export const fetcher = new KyAdapter(ky.create({ prefixUrl: API_ROUTE }));
