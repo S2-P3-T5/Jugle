3576040c42689c9d053f3133a1d21f22408f919c
eonpain <zmfltmwnsrl@naver.com>
2024-01-31 04:33:19 +0900

    feat: '거절(or 승인)하기' 버튼을 클릭하면 alert창이 뜨고 신청이 거절(or 승인)되는 기능 구현

--- src/components/noticeDetail/Buttons.tsx
+++ src/components/noticeDetail/Buttons.tsx
@@ -1,3 +1,5 @@
+import { MouseEventHandler } from "react";
+
 import { Button } from "@/components/ui/button";
 
 export const EditNoticeButton = () => {
@@ -10,9 +12,12 @@ export const EditNoticeButton = () => {
   );
 };
 
-export const RejectButton = () => {
+export const RejectButton: React.FC<applyReplyButtonProps> = ({ onClick }) => {
   return (
-    <Button className="flex h-[3.2rem] w-[6.9rem] gap-[0.8rem] rounded-[0.6rem] border-[0.1rem] border-primary bg-white px-[1.2rem] py-[0.8rem]">
+    <Button
+      onClick={onClick}
+      className="flex h-[3.2rem] w-[6.9rem] gap-[0.8rem] rounded-[0.6rem] border-[0.1rem] border-primary bg-white px-[1.2rem] py-[0.8rem]"
+    >
       <span className="text-center text-[1.2rem] font-normal not-italic leading-[1.6rem] text-primary">
         거절하기
       </span>
@@ -20,12 +25,21 @@ export const RejectButton = () => {
   );
 };
 
-export const ApproveButton = () => {
+export const ApproveButton: React.FC<applyReplyButtonProps> = ({ onClick }) => {
   return (
-    <Button className="flex h-[3.2rem] w-[6.9rem] gap-[0.8rem] rounded-[0.6rem] border-[0.1rem] border-blue-20 bg-white px-[1.2rem] py-[0.8rem]">
+    <Button
+      onClick={onClick}
+      className="flex h-[3.2rem] w-[6.9rem] gap-[0.8rem] rounded-[0.6rem] border-[0.1rem] border-blue-20 bg-white px-[1.2rem] py-[0.8rem]"
+    >
       <span className="text-center text-[1.2rem] font-normal not-italic leading-[1.6rem] text-blue-20">
         승인하기
       </span>
     </Button>
   );
+};
+
+//button type
+
+type applyReplyButtonProps = {
+  onClick: MouseEventHandler<HTMLButtonElement>;
 };
