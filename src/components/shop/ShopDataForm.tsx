aac713ef821f3a29fc0fe4411145b90b9890e53b
yspark <ys1008p@gmail.com>
2024-02-03 22:32:05 +0900

    design: 가게 정보 등록 / 편집 form 디자인 및 모바일 반응형 구현

--- src/components/shop/ShopDataForm.tsx
+++ src/components/shop/ShopDataForm.tsx
@@ -91,192 +91,216 @@ export default function ShopDataForm({
   const NUM_REGEX = /^\d+$/;
 
   return (
-    <>
-      <div className="flex justify-between">
-        <span>가게 정보</span>
+    <div className="mx-auto flex w-[35.1rem] flex-col gap-[2.4rem] pb-[8rem] pt-[4rem] tablet:py-[6rem]">
+      <div className="flex items-center justify-between">
+        <span className="text-[2rem] font-bold text-black">가게 정보</span>
         <BackPageButton />
       </div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            rules={{
-              required: {
-                value: true,
-                message: errorMessage.REQUIRED_SHOPNAME,
-              },
-            }}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>가게 이름*</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="입력"
-                    {...field}
-                    onBlur={() => form.trigger("name")}
-                  />
-                </FormControl>
-                <FormMessage className="absolute text-[1.2rem]" />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            rules={{
-              required: {
-                value: true,
-                message: errorMessage.REQUIRED_SHOPCATEGORY,
-              },
-            }}
-            name="category"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>분류*</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
+          <div className="flex flex-col gap-[2rem]">
+            <FormField
+              control={form.control}
+              rules={{
+                required: {
+                  value: true,
+                  message: errorMessage.REQUIRED_SHOPNAME,
+                },
+              }}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="relative flex flex-col">
+                  <FormLabel className="text-[1.6rem] leading-[2.6rem]">
+                    가게 이름*
+                  </FormLabel>
                   <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="입력" />
-                    </SelectTrigger>
+                    <Input
+                      className="text-[1.6rem] leading-[2.6rem]"
+                      placeholder="입력"
+                      {...field}
+                      onBlur={() => form.trigger("name")}
+                    />
                   </FormControl>
-                  <FormMessage className="absolute text-[1.2rem]" />
-                  <SelectContent>
-                    {CATEGORY.map((item) => {
-                      return (
+                  <FormMessage className="absolute bottom-[-2rem] text-[1.2rem]" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              rules={{
+                required: {
+                  value: true,
+                  message: errorMessage.REQUIRED_SHOPCATEGORY,
+                },
+              }}
+              name="category"
+              render={({ field }) => (
+                <FormItem className="relative flex flex-col">
+                  <FormLabel className="text-[1.6rem] leading-[2.6rem]">
+                    분류*
+                  </FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="text-[1.6rem] leading-[2.6rem]">
+                        <SelectValue placeholder="입력" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <FormMessage className="absolute bottom-[-2rem] text-[1.2rem]" />
+                    <SelectContent>
+                      {CATEGORY.map((item) => {
+                        return (
+                          <SelectItem key={item} value={item}>
+                            {item}
+                          </SelectItem>
+                        );
+                      })}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              rules={{
+                required: {
+                  value: true,
+                  message: errorMessage.REQUIRED_ADDRESS,
+                },
+              }}
+              name="address1"
+              render={({ field }) => (
+                <FormItem className="relative flex flex-col">
+                  <FormLabel className="text-[1.6rem] leading-[2.6rem]">
+                    주소*
+                  </FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="text-[1.6rem] leading-[2.6rem]">
+                        <SelectValue placeholder="입력" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <FormMessage className="absolute bottom-[-2rem] text-[1.2rem]" />
+                    <SelectContent>
+                      {ADDRESS.map((item) => (
                         <SelectItem key={item} value={item}>
                           {item}
                         </SelectItem>
-                      );
-                    })}
-                  </SelectContent>
-                </Select>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            rules={{
-              required: {
-                value: true,
-                message: errorMessage.REQUIRED_ADDRESS,
-              },
-            }}
-            name="address1"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>주소*</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              rules={{
+                required: {
+                  value: true,
+                  message: errorMessage.REQUIRED_DETAILED_ADDRESS,
+                },
+              }}
+              name="address2"
+              render={({ field }) => (
+                <FormItem className="relative flex flex-col">
+                  <FormLabel className="text-[1.6rem] leading-[2.6rem]">
+                    상세 주소*
+                  </FormLabel>
                   <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="입력" />
-                    </SelectTrigger>
+                    <Input
+                      className="text-[1.6rem] leading-[2.6rem]"
+                      placeholder="입력"
+                      {...field}
+                      onBlur={() => form.trigger("address2")}
+                    />
                   </FormControl>
-                  <FormMessage className="absolute text-[1.2rem]" />
-                  <SelectContent>
-                    {ADDRESS.map((item) => (
-                      <SelectItem key={item} value={item}>
-                        {item}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            rules={{
-              required: {
-                value: true,
-                message: errorMessage.REQUIRED_DETAILED_ADDRESS,
-              },
-            }}
-            name="address2"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>상세 주소*</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="입력"
-                    {...field}
-                    onBlur={() => form.trigger("address2")}
-                  />
-                </FormControl>
-                <FormMessage className="absolute text-[1.2rem]" />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            rules={{
-              required: {
-                value: true,
-                message: errorMessage.REQUIRED_HOURLYPAY,
-              },
-              pattern: {
-                value: NUM_REGEX,
-                message: errorMessage.REQUIRED_NUMBER,
-              },
-              min: {
-                value: 9860,
-                message: errorMessage.INVALID_HOURLYPAY,
-              },
-            }}
-            name="originalHourlyPay"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>기본 시급*</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="입력"
-                    {...field}
-                    onBlur={() => form.trigger("originalHourlyPay")}
-                  />
-                </FormControl>
-                <FormMessage className="absolute text-[1.2rem]" />
-              </FormItem>
-            )}
-          />
-          <FormItem>
-            <FormLabel htmlFor="imgSelector">
-              가게 이미지
-              <ShopImageCard imgURL={imgURL} />
-            </FormLabel>
-            <FormControl>
-              <Input
-                accept=".jpg, .jpeg, .png"
-                className="hidden"
-                id="imgSelector"
-                type="file"
-                onChange={handleInputImgFile}
-              />
-            </FormControl>
-          </FormItem>
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>가게 설명</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="입력"
-                    className="resize-none"
-                    {...field}
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
+                  <FormMessage className="absolute bottom-[-2rem] text-[1.2rem]" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              rules={{
+                required: {
+                  value: true,
+                  message: errorMessage.REQUIRED_HOURLYPAY,
+                },
+                pattern: {
+                  value: NUM_REGEX,
+                  message: errorMessage.REQUIRED_NUMBER,
+                },
+                min: {
+                  value: 9860,
+                  message: errorMessage.INVALID_HOURLYPAY,
+                },
+              }}
+              name="originalHourlyPay"
+              render={({ field }) => (
+                <FormItem className="relative flex flex-col">
+                  <FormLabel className="text-[1.6rem] leading-[2.6rem]">
+                    기본 시급*
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="text-[1.6rem] leading-[2.6rem]"
+                      placeholder="입력"
+                      {...field}
+                      onBlur={() => form.trigger("originalHourlyPay")}
+                    />
+                  </FormControl>
+                  <FormMessage className="absolute bottom-[-2rem] text-[1.2rem]" />
+                </FormItem>
+              )}
+            />
+            <FormItem className="relative flex flex-col">
+              <FormLabel
+                htmlFor="imgSelector"
+                className="text-[1.6rem] leading-[2.6rem]"
+              >
+                가게 이미지
+                <ShopImageCard imgURL={imgURL} />
+              </FormLabel>
+              <FormControl>
+                <Input
+                  accept=".jpg, .jpeg, .png"
+                  className="hidden"
+                  id="imgSelector"
+                  type="file"
+                  onChange={handleInputImgFile}
+                />
+              </FormControl>
+            </FormItem>
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem className="relative flex flex-col">
+                  <FormLabel className="text-[1.6rem] leading-[2.6rem]">
+                    가게 설명
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="입력"
+                      className="h-[15.3rem] resize-none rounded-[0.5rem] px-[2rem] py-[1.6rem] text-[1.6rem] leading-[2.6rem]"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
           <AlertDialog>
             <AlertDialogTrigger asChild>
-              <Button disabled={!form.formState.isValid} type="submit">
+              <Button
+                className="mt-[2.4rem] flex h-[4.8rem] w-[35.1rem] items-center justify-center px-[13.6rem] py-[1.4rem] text-[1.6rem] font-bold leading-[2rem]"
+                disabled={!form.formState.isValid}
+                type="submit"
+              >
                 {buttonText}
               </Button>
             </AlertDialogTrigger>
@@ -295,6 +319,6 @@ export default function ShopDataForm({
           </AlertDialog>
         </form>
       </Form>
-    </>
+    </div>
   );
 }
