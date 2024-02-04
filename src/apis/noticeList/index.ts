a7f849d4be5d58ce9dda153714c959f32d039aa3
dkile <dkile532@naver.com>
2024-02-04 23:56:41 +0900

    refactor: 공고 리스트 스키마 리팩터링

--- src/apis/noticeList/index.ts
+++ src/apis/noticeList/index.ts
@@ -1,14 +1,14 @@
 import { HTTPError } from "ky";
 
 import { fetcher } from "@/apis/fetcher";
+import { noticesResponseSchema } from "@/apis/noticeList/schema";
 import { apiRouteUtils } from "@/routes";
 
 export const getNoticeList = async () =>
   await fetcher
     .get(apiRouteUtils.NOTICES)
     .json()
-    // TODO : 스키마에러 수정 필요
-    // .then(apiResponseSchema.parse)
+    .then(noticesResponseSchema.parse)
     .catch((err: HTTPError) => {
       throw err;
     });
--- src/apis/noticeList/schema.ts
+++ src/apis/noticeList/schema.ts
@@ -1,45 +1,31 @@
 import { z } from "zod";
 
-import { createResponseSchema } from "@/apis/schema";
+import {
+  hrefSchema,
+  linksSchema,
+  noticeSchema,
+  paginationSchema,
+  shopSchema,
+} from "@/apis/schema";
 
-export const noticeListSchema = z.object({
-  offset: z.number(),
-  limit: z.number(),
-  count: z.number(),
-  hasNext: z.boolean(),
-  address: z.array(z.string()),
-  keyword: z.string(),
-
-  items: z.array(
+export const noticesResponseSchema = z
+  .object({
+    items: z.array(
+      z
+        .object({
+          item: z
+            .object({
+              shop: z.object({ item: shopSchema }).merge(hrefSchema),
+            })
+            .merge(noticeSchema),
+        })
+        .merge(linksSchema),
+    ),
+  })
+  .merge(paginationSchema)
+  .merge(
     z.object({
-      item: z.object({
-        id: z.string(),
-        hourlyPay: z.number(),
-        startsAt: z.string(),
-        workhour: z.number(),
-        description: z.string(),
-        closed: z.boolean(),
-        shop: z.object({
-          item: z.object({
-            id: z.string(),
-            name: z.string(),
-            category: z.string(),
-            address1: z.string(),
-            address2: z.string(),
-            description: z.string(),
-            imageUrl: z.string(),
-            originalHourlyPay: z.number(),
-          }),
-          href: z.string(),
-        }),
-      }),
+      address: z.string().array(),
+      keyword: z.string().optional(),
     }),
-  ),
-});
-
-export const apiResponseSchema = createResponseSchema(
-  z.object({
-    item: noticeListSchema,
-  }),
-);
-export type ApiResponseSchema = z.infer<typeof apiResponseSchema>;
+  );
