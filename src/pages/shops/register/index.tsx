c687a613384f55ec83fe04f87f378afa4df41d72
yspark <ys1008p@gmail.com>
2024-02-05 20:25:05 +0900

    feat: /shops/register 페이지 접근권한에 맞는 페이지 이동 구현

--- src/pages/shops/register/index.tsx
+++ src/pages/shops/register/index.tsx
@@ -1,5 +1,41 @@
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
+
+import { getUsersData } from "@/apis/shop";
+import EmployerLayout from "@/components/common/EmployerLayout";
 import ShopRegister from "@/components/shop/register/ShopRegister";
+import { getAccessTokenInStorage } from "@/helpers/auth";
+import { UserContext } from "@/providers/UserProvider";
+import { PAGE_ROUTES } from "@/routes";
 
 export default function ShopRegist() {
-  return <ShopRegister />;
+  const user = useContext(UserContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!getAccessTokenInStorage()) {
+      router.push(PAGE_ROUTES.SIGNIN);
+      return;
+    }
+    if (user?.type === "employee") {
+      router.push(PAGE_ROUTES.NOTICES);
+      return;
+    }
+    if (user) {
+      const getUserData = async () => {
+        const response: any = await getUsersData(user.id);
+        if (response.item.shop) {
+          router.push(PAGE_ROUTES.parseShopsURL(response.item.shop.id));
+          return;
+        }
+      };
+      getUserData();
+    }
+  }, [user]);
+
+  return (
+    <EmployerLayout>
+      <ShopRegister />;
+    </EmployerLayout>
+  );
 }
