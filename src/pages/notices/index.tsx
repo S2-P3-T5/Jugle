33d05de904996e7f405b7a0ccefb8538c38a82f6
yspark <ys1008p@gmail.com>
2024-02-06 15:25:09 +0900

    feat:notices 페이지 회원 타입에 맞는 GNB 표시

--- src/pages/notices/index.tsx
+++ src/pages/notices/index.tsx
@@ -1,9 +1,23 @@
+import { useContext } from "react";
+
+import EmployeeLayout from "@/components/common/EmployeeLayout";
+import EmployerLayout from "@/components/common/EmployerLayout";
 import NoticeListForm from "@/components/notices/NoticeListForm";
+import { UserContext } from "@/providers/UserProvider";
 
 export default function Notices() {
+  const user = useContext(UserContext);
   return (
     <>
-      <NoticeListForm />
+      {user?.type === "employer" ? (
+        <EmployerLayout>
+          <NoticeListForm />
+        </EmployerLayout>
+      ) : (
+        <EmployeeLayout>
+          <NoticeListForm />
+        </EmployeeLayout>
+      )}
     </>
   );
 }
