32c9cec31865d788066202817525fc1c57f9c3d8
yspark <ys1008p@gmail.com>
2024-02-08 16:31:08 +0900

    feat:공고 filter의 address selcetor 컴포넌트 구현

---
+++ src/components/notices/AddressSelectorsItem.tsx
@@ -0,0 +1,9 @@
+interface AddressSelectorsItemProps {
+  children: string;
+}
+
+export default function AddressSelectorsItem({
+  children,
+}: AddressSelectorsItemProps) {
+  return <div>{children}</div>;
+}
