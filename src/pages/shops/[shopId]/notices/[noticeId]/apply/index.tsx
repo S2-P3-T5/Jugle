67cb7bab0b17f56a62abd9fea075371fb7e288fd
eonpain <zmfltmwnsrl@naver.com>
2024-02-12 01:59:21 +0900

    fix: 공고상세페이지(로그인 상태)페이지에서 새로고침했을 때 생기는 오류 해결(완벽히 해결못하고 오류만 막음)

--- src/pages/shops/[shopId]/notices/[noticeId]/apply/index.tsx
+++ src/pages/shops/[shopId]/notices/[noticeId]/apply/index.tsx
@@ -19,6 +19,7 @@ import { useUserQuery } from "@/queries/user";
 import { apiRouteUtils, PAGE_ROUTES } from "@/routes";
 
 function NoticeDetailApply() {
+  const [mounted, setMounted] = useState<boolean>(false);
   const [recentNotices, setRecentNotices] = useState<
     { id: string; data: any }[]
   >([]);
@@ -30,6 +31,10 @@ function NoticeDetailApply() {
   const { user } = useUserQuery();
   const userProfile = useContext(UserContext);
   const userId = userProfile?.id;
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   useEffect(() => {
     if (!user?.id) router.push(PAGE_ROUTES.SIGNIN);
@@ -192,123 +197,128 @@ function NoticeDetailApply() {
   }, [refetch, applyStatus, applicationStatus]);
 
   return (
-    <EmployeeLayout>
-      <div className="flex w-full flex-col items-center justify-center tablet:w-[74.4rem] desktop:w-[144rem]">
-        <div className="flex w-full flex-col items-start gap-[1.2rem] bg-[#fafafa] px-[1.2rem] py-[4rem] tablet:w-full tablet:px-[3.2rem] tablet:py-[6rem] desktop:px-[23.8rem]">
-          <div className="flex w-full flex-col gap-[1.6rem] tablet:w-full">
-            <div className="inline-flex flex-col items-start gap-[0.8rem]">
-              <span className="text-[1.4rem] font-bold not-italic leading-normal text-primary tablet:text-[1.6rem]  ">
-                {shopOriginalData?.category}
-              </span>
-              <span className="text-[2rem] font-bold not-italic leading-normal text-black tablet:text-[2.8rem]">
-                {shopOriginalData?.name}
-              </span>
-            </div>
-            <div className="flex w-[35.1rem] flex-col items-start gap-[1.2rem] rounded-[1.2rem] border border-gray-20 bg-white p-[2rem] tablet:w-[68rem] tablet:gap-[1.6rem] tablet:p-[2.4rem] desktop:h-[35.6rem] desktop:w-[96.4rem] desktop:flex-row">
-              <div className="relative flex h-[15.8rem] w-[31.1rem] items-center justify-center tablet:h-[33.2rem] tablet:w-[63.2rem] desktop:h-[30.8rem] desktop:w-[55.4rem]">
-                <Image
-                  src={shopOriginalData?.imageUrl}
-                  layout="fill"
-                  objectFit="contain"
-                  alt="로고이미지"
-                />
-              </div>
-              <div className="flex flex-col items-start gap-[2.4rem] self-stretch">
-                <div className="flex flex-col items-start gap-[0.8rem] self-stretch tablet:gap-[1.2rem]">
-                  <div className="flex flex-col items-start gap-[0.8rem]">
-                    <span className="text-[1.4rem] font-bold not-italic leading-normal text-primary tablet:text-[1.6rem]  ">
-                      시급
-                    </span>
-                    <div className="flex w-full items-center gap-[0.4rem]">
-                      <span className="text-[2.4rem] font-bold not-italic leading-normal tracking-[0.048rem] text-black tablet:text-[2.8rem]">
-                        {shopNoticeData?.hourlyPay}원
-                      </span>
-                      {hourlyPay > originalHourlyPay && (
-                        <HighHourlyWageBadge
-                          className={""}
-                          increasePercentage={0}
-                          {...badgeProps}
-                        />
-                      )}
-                    </div>
-                  </div>
-                  <div className="flex items-center gap-[0.6rem]">
-                    <div className="relative flex h-[1.6rem] w-[1.6rem] items-center justify-center">
-                      <Image
-                        src="/icons/clock.svg"
-                        alt="시간 아이콘"
-                        layout="fill"
-                        objectFit="contain"
-                      />
-                    </div>
-                    <span className="text-[1.4rem] font-normal not-italic leading-[2.2rem] text-gray-50 tablet:text-[1.6rem]">
-                      {startDay} {startTime}:{minute}~{endTime}:{minute}(
-                      {shopOriginalData?.workhour}
-                      시간)
-                    </span>
-                  </div>
-                  <div className="flex items-center gap-[0.6rem]">
-                    <div className="relative flex h-[1.6rem] w-[1.6rem] items-center justify-center">
-                      <Image
-                        src="/icons/point.svg"
-                        alt="장소 아이콘"
-                        layout="fill"
-                        objectFit="contain"
-                      />
-                    </div>
-                    <span className="text-[1.4rem] font-normal not-italic leading-[2.2rem] text-gray-50 tablet:text-[1.6rem]">
-                      {shopOriginalData?.address1} {shopOriginalData?.address2}
-                    </span>
-                  </div>
-                  <span className="text-black-50 h-[6.6rem] scroll-auto text-[1.4rem] font-normal not-italic leading-[2.2rem] tablet:text-[1.6rem]">
-                    {shopOriginalData?.description}
+    mounted && (
+      <>
+        <EmployeeLayout>
+          <div className="flex w-full flex-col items-center justify-center tablet:w-[74.4rem] desktop:w-[144rem]">
+            <div className="flex w-full flex-col items-start gap-[1.2rem] bg-[#fafafa] px-[1.2rem] py-[4rem] tablet:w-full tablet:px-[3.2rem] tablet:py-[6rem] desktop:px-[23.8rem]">
+              <div className="flex w-full flex-col gap-[1.6rem] tablet:w-full">
+                <div className="inline-flex flex-col items-start gap-[0.8rem]">
+                  <span className="text-[1.4rem] font-bold not-italic leading-normal text-primary tablet:text-[1.6rem]  ">
+                    {shopOriginalData?.category}
+                  </span>
+                  <span className="text-[2rem] font-bold not-italic leading-normal text-black tablet:text-[2.8rem]">
+                    {shopOriginalData?.name}
                   </span>
                 </div>
-                {applyStatus === "pending" ? (
-                  <CancelDialog handleCancel={handleCancel} />
-                ) : (
-                  <ProfileRegistDialog
-                    handleApply={handleApply}
-                    profile={profile}
-                    pagingProfileRegist={pagingProfileRegist}
-                  />
-                )}
+                <div className="flex w-[35.1rem] flex-col items-start gap-[1.2rem] rounded-[1.2rem] border border-gray-20 bg-white p-[2rem] tablet:w-[68rem] tablet:gap-[1.6rem] tablet:p-[2.4rem] desktop:h-[35.6rem] desktop:w-[96.4rem] desktop:flex-row">
+                  <div className="relative flex h-[15.8rem] w-[31.1rem] items-center justify-center tablet:h-[33.2rem] tablet:w-[63.2rem] desktop:h-[30.8rem] desktop:w-[55.4rem]">
+                    <Image
+                      src={shopOriginalData?.imageUrl}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="로고이미지"
+                    />
+                  </div>
+                  <div className="flex flex-col items-start gap-[2.4rem] self-stretch">
+                    <div className="flex flex-col items-start gap-[0.8rem] self-stretch tablet:gap-[1.2rem]">
+                      <div className="flex flex-col items-start gap-[0.8rem]">
+                        <span className="text-[1.4rem] font-bold not-italic leading-normal text-primary tablet:text-[1.6rem]  ">
+                          시급
+                        </span>
+                        <div className="flex w-full items-center gap-[0.4rem]">
+                          <span className="text-[2.4rem] font-bold not-italic leading-normal tracking-[0.048rem] text-black tablet:text-[2.8rem]">
+                            {shopNoticeData?.hourlyPay}원
+                          </span>
+                          {hourlyPay > originalHourlyPay && (
+                            <HighHourlyWageBadge
+                              className={""}
+                              increasePercentage={0}
+                              {...badgeProps}
+                            />
+                          )}
+                        </div>
+                      </div>
+                      <div className="flex items-center gap-[0.6rem]">
+                        <div className="relative flex h-[1.6rem] w-[1.6rem] items-center justify-center">
+                          <Image
+                            src="/icons/clock.svg"
+                            alt="시간 아이콘"
+                            layout="fill"
+                            objectFit="contain"
+                          />
+                        </div>
+                        <span className="text-[1.4rem] font-normal not-italic leading-[2.2rem] text-gray-50 tablet:text-[1.6rem]">
+                          {startDay} {startTime}:{minute}~{endTime}:{minute}(
+                          {shopOriginalData?.workhour}
+                          시간)
+                        </span>
+                      </div>
+                      <div className="flex items-center gap-[0.6rem]">
+                        <div className="relative flex h-[1.6rem] w-[1.6rem] items-center justify-center">
+                          <Image
+                            src="/icons/point.svg"
+                            alt="장소 아이콘"
+                            layout="fill"
+                            objectFit="contain"
+                          />
+                        </div>
+                        <span className="text-[1.4rem] font-normal not-italic leading-[2.2rem] text-gray-50 tablet:text-[1.6rem]">
+                          {shopOriginalData?.address1}{" "}
+                          {shopOriginalData?.address2}
+                        </span>
+                      </div>
+                      <span className="text-black-50 h-[6.6rem] scroll-auto text-[1.4rem] font-normal not-italic leading-[2.2rem] tablet:text-[1.6rem]">
+                        {shopOriginalData?.description}
+                      </span>
+                    </div>
+                    {applyStatus === "pending" ? (
+                      <CancelDialog handleCancel={handleCancel} />
+                    ) : (
+                      <ProfileRegistDialog
+                        handleApply={handleApply}
+                        profile={profile}
+                        pagingProfileRegist={pagingProfileRegist}
+                      />
+                    )}
+                  </div>
+                </div>
+                <div className="flex h-[15.3rem] w-full flex-col items-start gap-[0.8rem] rounded-[1.2rem] bg-gray-10 p-[2rem] tablet:h-[14.8rem]">
+                  <span className="text-black-50 scroll-auto text-[1.4rem] font-bold not-italic leading-[2.2rem] tablet:text-[1.6rem]">
+                    공고 설명
+                  </span>
+                  <span className="text-black-50 scroll-auto text-[1.4rem] font-normal not-italic leading-[2.2rem] tablet:text-[1.6rem]">
+                    {shopNoticeData?.description}
+                  </span>
+                </div>
               </div>
             </div>
-            <div className="flex h-[15.3rem] w-full flex-col items-start gap-[0.8rem] rounded-[1.2rem] bg-gray-10 p-[2rem] tablet:h-[14.8rem]">
-              <span className="text-black-50 scroll-auto text-[1.4rem] font-bold not-italic leading-[2.2rem] tablet:text-[1.6rem]">
-                공고 설명
-              </span>
-              <span className="text-black-50 scroll-auto text-[1.4rem] font-normal not-italic leading-[2.2rem] tablet:text-[1.6rem]">
-                {shopNoticeData?.description}
-              </span>
+            <div className="flex w-full flex-col items-start gap-[1.6rem] bg-[#fafafa] px-[1.2rem] pb-[8rem] pt-[4rem]">
+              <h1 className="text-[2rem] font-bold not-italic leading-normal text-black">
+                최근에 본 공고
+              </h1>
+              <div className="grid grid-cols-2 gap-x-[0.8rem] gap-y-[1.6rem]">
+                {storedRecentNotices.map((notice: any) => (
+                  <div key={notice.id}>
+                    <Link
+                      href={PAGE_ROUTES.parseNotciesApplyURL(
+                        notice.shopdata.id,
+                        notice.noticedata.id,
+                      )}
+                    >
+                      <NoticeApplyItem
+                        item={notice.noticedata}
+                        shopData={notice.shopdata}
+                      />
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex w-full flex-col items-start gap-[1.6rem] bg-[#fafafa] px-[1.2rem] pb-[8rem] pt-[4rem]">
-          <h1 className="text-[2rem] font-bold not-italic leading-normal text-black">
-            최근에 본 공고
-          </h1>
-          <div className="grid grid-cols-2 gap-x-[0.8rem] gap-y-[1.6rem]">
-            {storedRecentNotices.map((notice: any) => (
-              <div key={notice.id}>
-                <Link
-                  href={PAGE_ROUTES.parseNotciesApplyURL(
-                    notice.shopdata.id,
-                    notice.noticedata.id,
-                  )}
-                >
-                  <NoticeApplyItem
-                    item={notice.noticedata}
-                    shopData={notice.shopdata}
-                  />
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </EmployeeLayout>
+        </EmployeeLayout>
+      </>
+    )
   );
 }
 
