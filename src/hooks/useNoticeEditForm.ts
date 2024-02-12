7fd51e9532926c884ff7af3d43ddb3db6a5266a5
eonpain <zmfltmwnsrl@naver.com>
2024-02-12 19:42:19 +0900

    feat: 사장님 - ( 공고 상세 페이지 ): 공고 편집 시 input 내에 공고 등록시의 값을 기본값으로 설정하는 기능 구현

--- src/hooks/useNoticeEditForm.ts
+++ src/hooks/useNoticeEditForm.ts
@@ -5,9 +5,18 @@ import { useNoticeEdit } from "@/queries/shop";
 import { FormRules } from "@/types/form";
 import { NoticeRegistFormField } from "@/types/shop";
 
-export default function useNoticeEditForm(shopId: string, noticeId: string) {
+export default function useNoticeEditForm(
+  shopId: string,
+  noticeId: string,
+  currentNoticeData: any,
+) {
   const form = useForm<NoticeRegistFormField>({
-    values: { hourlyPay: 0, startsAt: "", workhour: 0, description: "" },
+    values: {
+      hourlyPay: currentNoticeData.hourlyPay,
+      startsAt: currentNoticeData.startsAt,
+      workhour: currentNoticeData.workhour,
+      description: currentNoticeData.description,
+    },
   });
   const { mutate } = useNoticeEdit(shopId, noticeId);
 
